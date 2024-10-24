49b4f09cc1f55bb3cd44ae6db170a2078447db06
MaciejCwalina <150276683+MaciejCwalina@users.noreply.github.com>
2024-10-24 11:06:26 +0200

    Slowly getting ready for a re-write.

---
+++ TSharkWrapper/TShark.go
@@ -0,0 +1,33 @@
+package TSharkWrapper
+
+import (
+	"log"
+	"os/exec"
+)
+
+type TShark struct {
+	tsharkCmd *exec.Cmd
+}
+
+func NewTShark() *TShark {
+	cmd := exec.Command("tshark", "-e ip.addr -e frame.len -i any -T fields")
+	return &TShark{tsharkCmd: cmd}
+}
+
+func (tshark *TShark) Run() {
+	err := tshark.tsharkCmd.Run()
+	if err != nil {
+		log.Fatal("Unable to start tshark due to ", err)
+	}
+}
+
+func (tshark *TShark) RedirectOutputToChannelAsync(unParsedDataChannel chan string) {
+	for {
+		bytes, err := tshark.tsharkCmd.Output()
+		if err != nil {
+			log.Fatal("Unable to get the output of tshark due to: ", err.Error())
+		}
+
+		unParsedDataChannel <- string(bytes)
+	}
+}
--- main.go
+++ main.go
@@ -3,61 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-	"strings"
-	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/redis/go-redis/v9"
 )
 
 func ParseData(validDataChannel chan []byte, packetChannel chan []Packet) {
-	go func() {
-		var bytes []byte
-		for {
-			select {
-			case b, ok := <-validDataChannel:
-				if ok {
-					bytes = b
-				} else {
-					log.Fatal("Channel is closed this should never happen")
-				}
 
-			default:
-				continue
-			}
-
-			start := time.Now()
-			packetInfo := string(bytes)
-			packetInfoSplit := strings.Split(packetInfo, "\n")
-			packets := []Packet{}
-			for _, packet := range packetInfoSplit {
-				packetSplit := strings.Split(packet, " ")
-				lengthPacketSplit := len(packetSplit)
-				if lengthPacketSplit < 8 {
-					continue
-				}
-
-				var packet Packet
-				var err error
-				if packetSplit[5] == "UDP," {
-					packet, err = CreateUDPPacket(packetSplit)
-				} else {
-					packet, err = CreateTCPPacket(packetSplit)
-				}
-
-				if err != nil {
-					continue
-				}
-
-				packets = append(packets, packet)
-			}
-
-			log.Println(time.Since(start))
-			if len(packetChannel) < cap(packetChannel) {
-				packetChannel <- packets
-			}
-		}
-	}()
 }
 
 func RecordMetrics(packetsChannel chan []Packet) {
@@ -79,17 +31,12 @@ var (
 )
 
 func main() {
-	httpServer := HttpServer{
-		"tcp",
-		":5145",
-	}
+	//listener := httpServer.StartHttpServer()
+	//conn := httpServer.HandleIncomingConnections(listener)
 
-	listener := httpServer.StartHttpServer()
-	conn := httpServer.HandleIncomingConnections(listener)
 	validDataChannel := make(chan []byte, 128)
 	packetChannel := make(chan []Packet, 128)
-	httpServer.ReadAllBytesFromClient(validDataChannel, conn)
-
+	//httpServer.ReadAllBytesFromClient(validDataChannel, conn)
 	ParseData(validDataChannel, packetChannel)
 	RecordMetrics(packetChannel)
 	http.Handle("/metrics", promhttp.Handler())
